9cafce06cffceb07d8af5b699048d749799dd861
riffiy <riffiy.6135@gmail.com>
2023-02-28 10:00:00 +0000

    chapter5

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-/*global $*/
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+/* global $*/
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
